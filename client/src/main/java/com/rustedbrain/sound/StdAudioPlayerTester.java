091f85dac9e88d93f7e68d358a8fb36d6e220d49
RustedBrain <mogtarmogtar@gmail.com>
2016-03-03 09:14:33 +0200

    VERSION 0.6

---
+++ client/src/main/java/com/rustedbrain/sound/StdAudioPlayerTester.java
@@ -0,0 +1,25 @@
+package com.rustedbrain.sound;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+/**
+ * For manual testing of audio file playback
+ */
+public class StdAudioPlayerTester {
+
+    public static void main(String[] args) throws IOException, UnsupportedAudioFileException {
+        final AudioPlayer player = new AudioPlayerFactory().get();
+
+        final String mp3 = "D:\\Media\\Music\\Ego Likeness - New Legion EP (2015)\\07. New Legion (Third Stage Remix by Stoneburner).mp3";
+
+        playPath(player, mp3);
+    }
+
+    private static void playPath(AudioPlayer player, String path) throws IOException, UnsupportedAudioFileException {
+        final URL url = Paths.get(path).toUri().toURL();
+        player.play(url);
+    }
+}
